56ec31214ba63af7cc22a57e5d8291c88045b57a
Fahad-Rafiq06 <73123765+Fahad-Rafiq06@users.noreply.github.com>
2023-05-03 21:30:40 +0500

    Milli Seconds and seconds done

---
+++ app.js
@@ -0,0 +1,51 @@
+// function timer(){
+//     console.log("Running..")
+// }
+
+// setInterval(timer, 2000)
+
+// function timeout(){
+//     console.log("Running...")
+// }
+
+// setTimeout(timeout, 2000)
+
+// var count = 0;
+// var interval;
+
+// function timer(){
+//     count++;
+    
+//     console.log(count)
+// }
+// interval = setInterval(timer, 1000);
+
+// setTimeout(function(){
+//     clearInterval(interval)
+// },5000)
+
+// ================ Stop Watch Code begins Now ==============
+
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+
+var interval;
+
+function timer(){
+    msec++;
+    msecHeading.innerHTML = msec;
+    // console.log(msech)
+    if(msec >= 100){
+        sec++;
+        secHeading.innerHTML = sec
+        msec = 0
+    }
+
+}
+
+interval = setInterval(timer, 10)
